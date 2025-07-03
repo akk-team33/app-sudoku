61c106ce6f1a520695c359a556968614713a1ffa
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-07-03 21:19:59 +0200

    Removed decompiler comment

--- sudoku-main/src/main/java/de/team33/messaging/simplex/Relay.java
+++ sudoku-main/src/main/java/de/team33/messaging/simplex/Relay.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package de.team33.messaging.simplex;
 
 public interface Relay<MSG> extends Register<MSG> {
--- sudoku-main/src/main/java/de/team33/sudoku/Setup.java
+++ sudoku-main/src/main/java/de/team33/sudoku/Setup.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package de.team33.sudoku;
 
 import de.team33.messaging.simplex.Sender;
